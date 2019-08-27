8d1993c611fe3042c84f67c1b367cc322fb9c208
erdalaydin <erdalaydin92@hotmail.com>
2019-08-27 10:02:22 +0200

    tested test

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import { BrowserRouter } from 'react-router-dom'
 import store from './store'
 import {Provider} from 'react-redux'
 
+
+
+////TESTTTT
+
 ReactDOM.render(
     <Provider store={store}> 
       <BrowserRouter>
