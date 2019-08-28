e770391e6937ca356f1444233aef2ab6db2f9a31
erdalaydin <erdalaydin92@hotmail.com>
2019-08-28 09:49:30 +0200

    edited descriptions

--- src/components/OverView.js
+++ src/components/OverView.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
-import DescriptionOfFirstGame  from "./descriptionOfFirstGame"
-import DescriptionOfSecondGame from "./descriptionOfSecondGame"
-import DescriptionOfThirdGame from "./descriptionOfThirdGame"
+import {DescriptionOfFirstGame, DescriptionOfSecondGame, DescriptionOfThirdGame} from "./descriptionsOfGames"
+
 import "./overView.css"
 import { Link } from 'react-router-dom';
 
 class OverView extends Component {
     render() {
-        // const descriptionsOfFirstGame = "In this game you'll get 15 questions. You see a picture of a dog and you have to choose the right dog breed.Easy: you start with 3 breeds and 3 possible answers. After 5 correct answers the game gets more difficult (3 more breeds will be added)."
-        
+       
         return (
             <div>
                 <div>
+                    
                 <Link to='./practice'><h1 className="header">Practice</h1></Link>
+               
                  </div>
                  <div className="buttons">
                      <Link to='/gameone'><button type="button" className="btn btn-primary btn-lg">First Game</button></Link>
-                     <div class="divider"/>
+                     <div className="divider"/>
                      <Link to='/gametwo'><button type="button" className="btn btn-primary btn-lg">Second Game</button></Link>
-                     <div class="divider"/>
+                     <div className ="divider"/>
                      <Link to='/gamethree'> <button type="button" className="btn btn-primary btn-lg">Third Game</button></Link>
                  </div>
                  <div className="descriptions">
