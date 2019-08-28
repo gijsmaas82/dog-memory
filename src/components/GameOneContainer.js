875453bd1bf82634dafeffcc80aa50b7f899d4e4
gijsmaas82 <gijsmaas82@outlook.com>
2019-08-28 17:08:44 +0200

    game-logic

--- src/components/GameOneContainer.js
+++ src/components/GameOneContainer.js
@@ -25,12 +25,13 @@ function shuffle(array) {
 // Used like so
 var arr = [2, 11, 37, 42];
 arr = shuffle(arr);
-console.log(arr)
+
 
 
 class GameOneContainer extends Component {
   state = {
     question: 0,
+    streak: 0,
     points: 0,
     breeds: [],
     rightArray: [],
@@ -46,12 +47,13 @@ class GameOneContainer extends Component {
     const newDogs = [this.props.dogs[Math.floor(Math.random() * this.props.dogs.length)],
       this.props.dogs[Math.floor(Math.random() * this.props.dogs.length)],
       this.props.dogs[Math.floor(Math.random() * this.props.dogs.length)]]
-    this.setState({ breeds: this.state.breeds.concat(newDogs)})   
+    this.setState({ breeds: this.state.breeds.concat(newDogs)})
+    console.log('hello')   
   }
 
   getAnswers = () => {
     
-    const rightDog = this.state.breeds[Math.floor(Math.random()* (this.state.question % this.state.breeds.length))]
+    const rightDog = this.state.breeds[Math.floor(this.state.question % this.state.breeds.length)]
     const rıghtImage = request(`https://dog.ceo/api/breed/${encodeURIComponent(rightDog)}/images/random`)
       
     const wrongImage1 = request(`https://dog.ceo/api/breeds/image/random`)
@@ -66,10 +68,31 @@ class GameOneContainer extends Component {
           const shuffleArray = this.state.rightArray.map(url => url)
          this.setState({ shuffleArray: shuffle(shuffleArray)})
       })
-    
-      
   }
 
+  checkAnswer = (event) => {
+    if(event.target.id === this.state.rightArray[0] && this.state.streak === 4) {
+      this.setState({ question: this.state.question + 1, 
+        streak: 0, 
+        points: this.state.points + 1, 
+        rightArray: [], 
+        shuffleArray: [] })
+      this.getRandomBreeds()
+    } else if (event.target.id === this.state.rightArray[0]) {
+      this.setState({ question: this.state.question + 1, 
+        streak: this.state.streak + 1, 
+        points: this.state.points + 1, 
+        rightArray: [], 
+        shuffleArray: [] })
+      
+    } else {
+      this.setState({question: this.state.question + 1, 
+        streak: 0,
+        rightArray: [], 
+        shuffleArray: [] })
+    }
+    
+  }
 
   componentDidMount() {
     
@@ -80,21 +103,25 @@ class GameOneContainer extends Component {
     
     return (
       <div>
-        
+        <h2>User: {this.props.name} points: {this.state.points}/15 streak: {this.state.streak}</h2> 
+        {this.state.question === 2 && <h1>congratulations!</h1>}       
         {this.state.question === 0 && <button onClick={this.firstQuestion} > Click for the first question </button>}
         {this.state.question !== 0 && 
         <div>
           <h1>question: {this.state.question}</h1>
-          <h2> Click on the right picture of the {this.state.breeds[Math.floor(Math.random()* (this.state.question % this.state.breeds.length))]}</h2>
+          <h2> Click on the right picture of the {this.state.breeds[Math.floor(this.state.question % this.state.breeds.length)]}</h2>
         </div>}
         <div>
           
           {this.state.rightArray.length === 0 && this.state.question !== 0 ? 
           <button onClick={this.getAnswers}>click for answers</button> : ''}
           {this.state.rightArray.length !== 0 && <div>
-            <img src={this.state.shuffleArray[0]} alt="doggie" />
-            <img src={this.state.shuffleArray[1]} alt="doggie" />
-            <img src={this.state.shuffleArray[2]} alt="doggie" />
+            <img id={this.state.shuffleArray[0]} src={this.state.shuffleArray[0]} alt="doggie" 
+            onClick={this.checkAnswer} />
+            <img id={this.state.shuffleArray[1]} src={this.state.shuffleArray[1]} alt="doggie"
+            onClick={this.checkAnswer} />
+            <img id={this.state.shuffleArray[2]} src={this.state.shuffleArray[2]} alt="doggie" 
+            onClick={this.checkAnswer} />
           </div>}
         </div>
       </div>
@@ -104,7 +131,8 @@ class GameOneContainer extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    dogs: state.dogs
+    dogs: state.dogs,
+    name: state.login
   }
 }
 
